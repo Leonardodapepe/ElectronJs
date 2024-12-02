1ff4e0296da045ee248b36de31fa52278a5a2374
Robin.Hallgren <robbe@hallgren.fi>
2024-12-02 09:59:16 +0200

    ovn3 done

---
+++ Ovn3/sys_info/src/index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Use preload for secure access to Node.js
+      nodeIntegration: true,
+      contextIsolation: true,
+    },
+  });
+
+  mainWindow.loadFile('src/index.html');
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+      },
+    });
+
+    mainWindow.loadFile('src/index.html');
+  }
+});
